da776b5c57a8d934abf98deb71564c7b15e73a36
sonu <sonu855336@gmail.com>
2023-07-01 10:32:30 +0530

    src

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import React, { useState } from 'react'
-import CustomerPage from './Component/CustomerPage'
+import React, { useState } from "react";
+import CustomerPage from "./Component/CustomerPage";
 
-
-import { Route,Switch,Redirect } from 'react-router-dom'
-import './App.css'
-import LoginPage from './Component/LoginPage'
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./Component/LoginPage";
 
 function App() {
-  const [token,setToken] = useState('')
-  const [ipadd,setipadd] = useState('')
-   const getTokenHandler=(logintoken,ipaddress)=>{
-setToken(logintoken)
- 
-setipadd(ipaddress)
-console.log(ipaddress,'from app component')
-   }
+  const [token, setToken] = useState("");
+  const [ipadd, setipadd] = useState("");
+  const getTokenHandler = (logintoken, ipaddress) => {
+    setToken(logintoken);
+
+    setipadd(ipaddress);
+    console.log(ipaddress, "from app component");
+  };
   return (
-    <div  className="app">
-    <Switch>
-    {/* <Route path="/Popup"  exact>
-       <PopUp  getip={getip}/>
-    </Route> */}
-    <Route path="/Loginpage">
-      <LoginPage getTokenHandler={getTokenHandler} />
-    </Route>
-    <Route path="/CustomerPage">
-<CustomerPage  token={token} ipadd ={ipadd}/>
-    </Route>
-    <Route path="*">
-      <Redirect to='/Loginpage'/>
-    </Route>
-    </Switch>
-       
+    <div className="app">
+      <Switch>
+        <Route path="/Loginpage">
+          <LoginPage getTokenHandler={getTokenHandler} />
+        </Route>
+        <Route path="/CustomerPage">
+          <CustomerPage token={token} ipadd={ipadd} />
+        </Route>
+        <Route path="*">
+          <Redirect to="/Loginpage" />
+        </Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
