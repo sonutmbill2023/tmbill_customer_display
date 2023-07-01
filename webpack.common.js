9a5b6d59a9dc1c435da313b42b8bbb22ef0e811b
sonu <sonu855336@gmail.com>
2023-07-01 10:43:39 +0530

    webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,60 +1,56 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  
-  target: 'electron-renderer',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+
+  target: "electron-renderer",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [[
-              '@babel/preset-env', {
-                targets: {
-                  esmodules: true
-                }
-              }],
-              '@babel/preset-react']
-          }
-        }
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
       },
       {
         test: [/\.s[ac]ss$/i, /\.css$/i],
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-          
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
           },
         ],
       },
       {
         test: /\.(mp3|wav)$/,
-        loader: 'file-loader'
-    }
-    ]
+        loader: "file-loader",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: [".js"],
   },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'build', 'js'),
+    filename: "app.js",
+    path: path.resolve(__dirname, "build", "js"),
   },
 };
