cc561c86dc0709dbbe376bf3d88598dd4fd1c6a6
sonutmbill2023 <sonu@tmbill.com>
2023-07-28 15:15:15 +0530

    main.js

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-const path = require("path");
+  const path = require("path");
 
 const { app, BrowserWindow, dialog, nativeImage } = require("electron");
+ 
 
-let win;
+let win = null;
 
 function createWindow() {
   win = new BrowserWindow({
+     
     width: 800,
     height: 600,
-    autoHideMenuBar: true,
-    icon: __dirname + "/icon.ico",
+ 
+     icon: __dirname + "/icon.ico",
+    
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: true,
+      contextIsolation:false,
+      
+      
     },
+    webSecurity: false,
+    
+     
   });
 
   win.loadFile("index.html");
 
+    //win.setMenuBarVisibility(false);
+
   return win;
 }
+
+require("electron-reload")(__dirname, {
+  electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+});
 
 app.on("ready", () => {
   win = createWindow();
@@ -30,7 +44,7 @@ app.on("ready", () => {
     const { response } = await dialog.showMessageBox(win, {
       type: "question",
       buttons: ["Yes", "No"],
-      title: "Confirm Exit",
+      title: "TMBill",
       message: "Are you sure you want to exit?",
     });
 
@@ -39,6 +53,18 @@ app.on("ready", () => {
     }
   });
 });
+
+// app.on('activate', () => {
+//   // Check if the main window exists, and if not, create it.
+//   if ( win === null) {
+//     win = createWindow();
+//   } else {
+//     // If the main window already exists, just focus it instead of creating a new one.
+//     win.focus();;
+//   }
+// });
+
+ 
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
